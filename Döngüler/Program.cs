1e50f99062c599283387dba8a20108b4302be260
MERVE BÜŞRA <mervebusradurmus19@gmail.com>
2021-03-24 15:53:48 +0300

    Add project files.

---
+++ Döngüler/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Döngüler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kurs1 = "Yazılım Geliştirici Yetiştirme Kampı";
+            string kurs2 = "Programlamaya Başlangıç için Temel Kurs";
+            string kurs3 = "Java";
+            string kurs4 = "Python";
+
+            //yukarıdakini döngü içinde döndüremem. Bunun için array şeklinde yazıyoruz.
+            string[] kurslar = new string[] { "Yazılım Geliştirici Yetiştirme Kampı", "Programlamaya Başlangıç için Temel Kurs" , "Java","Python" };
+
+
+            for (int i = 0; i < kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+
+            Console.WriteLine("For Bitti");
+            foreach (var kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+
+            Console.WriteLine("Sayfa Sonu - Footer");
+        }
+    }
+}
