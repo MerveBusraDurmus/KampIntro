6bd7a84cde9e72f75cd31f364447fe0acfb71d06
MERVE BÜŞRA <mervebusradurmus19@gmail.com>
2021-03-25 21:18:02 +0300

    commit-3

---
+++ DegerVeReferansTipler/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DegerVeReferansTipler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi1 = 10;
+            int sayi2 = 30;
+            sayi1 = sayi2;
+            sayi2 = 65;
+            //sayi1?  30 olur çünkü değer tipidir.
+
+
+            int[] sayilar1 = new int[] { 10, 20, 30 };
+            int[] sayilar2 = new int[] { 100, 200, 300 };
+            sayilar1 = sayilar2;  //sayilar1'in referansı da sayilar2'nin referansı ile aynıdır.
+            sayilar2[0] = 999;
+            //sayilar1[0] ?  999 dur çünkü değer tipidir.
+
+            //değer tiplerde değer atanır referans tiplerde adres atanır.
+            //int,decimal,float,double,bool=değer tip
+            //array,class,interface=referans tip
+        }
+    }
+}
