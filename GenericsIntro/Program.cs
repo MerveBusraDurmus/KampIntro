dfba85c27ef1f5febc550bc1bca3ff73037a19b1
MERVE BÜŞRA <mervebusradurmus19@gmail.com>
2021-03-26 00:18:46 +0300

    commit-4

---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("Engin");
+            isimler.Add("Engin");
+            isimler.Add("Derin");
+            isimler.Add("Engin");
+            isimler.Add("Engin");
+            isimler.Add("Engin");
+
+
+            Console.WriteLine(isimler.Length);
+
+            foreach (var isim in isimler.Items)
+            {
+                Console.WriteLine(isim);
+            }
+
+
+
+        }
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.CategoryId = 2;
+            product1.ProductName = "Masa";
+            product1.UnitPrice = 500;
+            product1.UnitsInStock = 3;
+
+
+            Product product2 = new Product {Id=2,CategoryId=5,UnitsInStock=5,ProductName="Kalem",UnitPrice=35};
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+
+
+            productManager.Topla2(3, 6);
+
+            int islemSonucu = productManager.Topla(3, 6);
+            Console.WriteLine(islemSonucu*2);
+        }
+    }
+}
