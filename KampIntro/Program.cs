1c817d7ea6845404719ac79d4ad1baecce1107f3
MERVE BÜŞRA <mervebusradurmus19@gmail.com>
2021-03-25 21:20:56 +0300

    .

--- KampIntro/Program.cs
+++ KampIntro/Program.cs
@@ -6,7 +6,7 @@ namespace KampIntro
     {
         static void Main(string[] args)
         {
-            //type safety - tiğ güvenliği
+            //type safety - tip güvenliği
             //Do not repeat yourself - kendini tekrarlama
             //değer tutucu , alias
 
