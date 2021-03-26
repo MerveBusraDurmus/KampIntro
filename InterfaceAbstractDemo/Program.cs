36a97c0c4b2575314b3a0042fd77ba5122d9a40d
MERVE BÜŞRA <mervebusradurmus19@gmail.com>
2021-03-27 01:07:37 +0300

    commit-6

---
+++ InterfaceAbstractDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Adapters;
+using InterfaceAbstractDemo.Concrete;
+using System;
+
+namespace InterfaceAbstractDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseCustomerManager customerManager = new StarbucksCustomerManager(new MernisServiceAdapter());
+            customerManager.Save(new Entities.Customer { DateOfBirth = new DateTime(1990, 5, 12), FirstName = "Merve Büşra", LastName = "Durmuş", NationalityId = "1111111111111" });
+            Console.ReadLine();
+        }
+    }
+}
